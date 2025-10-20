d86f26510d004ac21bb1a9c84791acdcb75e9f24
speedydelete <speedykorok777@gmail.com>
2025-10-20 18:53:31 -0400

    bruh

--- src/identify.ts
+++ src/identify.ts
@@ -79,6 +79,7 @@ function verifyType(p: Pattern, type: PartialIdentified): boolean {
     return true;
 }
 
+// @ts-ignore
 function mapMinmax(p: MAPPattern, type: PartialIdentified): {min: string, max: string} {
     if (p.ruleSymmetry === 'D8') {
         let [b, s] = arrayToTransitions(p.trs, TRANSITIONS);
@@ -132,7 +133,21 @@ function mapMinmax(p: MAPPattern, type: PartialIdentified): {min: string, max: s
 }
 
 
-const ROUND_CONSTANTS = [0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821, 0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8, 0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a, 0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70, 0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665, 0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391];
+const MD5_CONSTANTS = [0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821, 0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8, 0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a, 0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70, 0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665, 0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391];
+
+const MD5_SHIFTS = [
+    7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+    5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
+    4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+    6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21,
+];
+
+const MD5_BLOCKS = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+    1, 6, 11, 0, 5, 10, 15, 4, 9, 14, 3, 8, 13, 2, 7, 12,
+    5, 8, 11, 14, 1, 4, 7, 10, 13, 0, 3, 6, 9, 12, 15, 2,
+    0, 7, 14, 5, 12, 3, 10, 1, 8, 15, 6, 13, 4, 11, 2, 9,
+];
 
 function md5(data: Uint8Array): Uint32Array {
     let out = new Uint32Array(4);
@@ -141,12 +156,16 @@ function md5(data: Uint8Array): Uint32Array {
     out[2] = 0xfedcba98;
     out[3] = 0x76543210;
     let blockCount = Math.ceil(data.length / 64);
+    if (blockCount === 0) {
+        blockCount = 1;
+    }
     if (data.length % 64 >= 56) {
         blockCount++;
     }
     let padded = new Uint8Array(blockCount * 64);
     padded.set(data);
     padded[data.length] = 128;
+    console.log(padded);
     let blocks = new DataView(padded.buffer);
     blocks.setBigUint64(padded.length - 8, BigInt(data.length), true);
     for (let block = 0; block < blockCount; block++) {
@@ -155,7 +174,21 @@ function md5(data: Uint8Array): Uint32Array {
         let c = out[2];
         let d = out[3];
         for (let i = 0; i < 64; i++) {
-
+            let f: number;
+            if (i < 20) {
+                f = (b & c) | (~b & d);
+            } else if (i < 40) {
+                f = (b & c) | (b & ~d);
+            } else if (i < 60) {
+                f = b ^ c ^ d;
+            } else {
+                f = b ^ (c | ~d);
+            }
+            f = (f + a + MD5_CONSTANTS[i] + blocks.getUint32((block * 4 + MD5_BLOCKS[i]) * 4)) | 0;
+            a = d;
+            d = c;
+            c = b;
+            b += (f << MD5_SHIFTS[i]) | (f >>> (-MD5_SHIFTS[i] & ((1 << 31) * 2 - 1)));
         }
         out[0] += a;
         out[1] += b;
@@ -164,6 +197,8 @@ function md5(data: Uint8Array): Uint32Array {
     }
     return out;
 }
+
+console.log(Array.from(md5(new Uint8Array(0))).map(x => x.toString(16).padStart(8, '0')).join(''));
 
 export function identify(p: Pattern, limit: number): Identified {
     let type = findType(p, limit);
