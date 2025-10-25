7becb5f453663abe044d38627824402146480d94
speedydelete <speedykorok777@gmail.com>
2025-10-25 14:43:52 -0400

    fix md5

--- src/identify.ts
+++ src/identify.ts
@@ -1,6 +1,6 @@
 
+import {buffer} from 'stream/consumers';
 import {Pattern} from './pattern.js';
-import {arrayToTransitions, MAPPattern, TRANSITIONS, unparseTransitions, VALID_TRANSITIONS} from './map.js';
 
 
 export interface Identified {
@@ -14,8 +14,6 @@ export interface Identified {
     pops: number[];
     hashes: number[];
     phases: Pattern[];
-    min: string;
-    max: string;
 }
 
 type PartialIdentified = Omit<Identified, 'rle' | 'apgcode' | 'desc' | 'min' | 'max'>;
@@ -67,71 +65,6 @@ function findType(p: Pattern, limit: number): PartialIdentified {
 }
 
 
-function verifyType(p: Pattern, type: PartialIdentified): boolean {
-    for (let i = 0; i < (type.disp ? type.period : 8 * type.period); i++) {
-        p.runGeneration();
-        p.shrinkToFit();
-        if (p.hash32() !== type.hashes[i] && p.population !== type.pops[i] && !p.isEqual(type.phases[i])) {
-            return false;
-        }
-    }
-    return true;
-}
-
-// @ts-ignore
-function mapMinmax(p: MAPPattern, type: PartialIdentified): {min: string, max: string} {
-    if (p.ruleSymmetry === 'D8') {
-        let [b, s] = arrayToTransitions(p.trs, TRANSITIONS);
-        let minB = b.slice();
-        let minS = s.slice();
-        let maxB = b.slice();
-        let maxS = s.slice();
-        for (let tr in TRANSITIONS) {
-            let q = p.copy();
-            q.trs = q.trs.slice();
-            if (b.includes(tr)) {
-                for (let x of TRANSITIONS[tr]) {
-                    q.trs[x] = 0;
-                }
-                if (verifyType(q, type)) {
-                    minB = minB.filter(x => x !== tr);
-                }
-            } else {
-                for (let x of TRANSITIONS[tr]) {
-                    q.trs[x] = 1;
-                }
-                if (verifyType(q, type)) {
-                    maxB.push(tr);
-                }
-            }
-            q = p.copy();
-            q.trs = q.trs.slice();
-            if (s.includes(tr)) {
-                for (let x of TRANSITIONS[tr]) {
-                    q.trs[x] = 0;
-                }
-                if (verifyType(q, type)) {
-                    minS = minS.filter(x => x !== tr);
-                }
-            } else {
-                for (let x of TRANSITIONS[tr]) {
-                    q.trs[x] = 1;
-                }
-                if (verifyType(q, type)) {
-                    maxS.push(tr);
-                }
-            }
-        }
-        return {
-            min: 'B' + unparseTransitions(minB, VALID_TRANSITIONS, false) + '/S' + unparseTransitions(minS, VALID_TRANSITIONS, false),
-            max: 'B' + unparseTransitions(minB, VALID_TRANSITIONS, false) + '/S' + unparseTransitions(maxS, VALID_TRANSITIONS, false),
-        }
-    } else {
-
-    }
-}
-
-
 const MD5_CONSTANTS = [0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821, 0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8, 0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a, 0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70, 0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665, 0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391];
 
 const MD5_SHIFTS = [
@@ -148,12 +81,20 @@ const MD5_BLOCKS = [
     0, 7, 14, 5, 12, 3, 10, 1, 8, 15, 6, 13, 4, 11, 2, 9,
 ];
 
-function md5(data: Uint8Array): Uint32Array {
+function debug(msg: string, data: Uint8Array | Uint32Array): void {
+    console.log(msg, Array.from(new Uint8Array(data.buffer)).map(x => (x >>> 0).toString(16).padStart(2, '0')).join(' '));
+}
+
+function debug2(msg: string, ...nums: number[]): void {
+    console.log(msg, nums.map(x => (x >>> 0).toString(16).padStart(8, '0')).join(' '));
+}
+
+function md5(data: Uint8Array): Uint8Array {
     let out = new Uint32Array(4);
-    out[0] = 0x01234567;
-    out[1] = 0x89abcdef;
-    out[2] = 0xfedcba98;
-    out[3] = 0x76543210;
+    out[0] = 0x67452301;
+    out[1] = 0xefcdab89;
+    out[2] = 0x98badcfe;
+    out[3] = 0x10325476;
     let blockCount = Math.ceil(data.length / 64);
     if (blockCount === 0) {
         blockCount = 1;
@@ -164,49 +105,61 @@ function md5(data: Uint8Array): Uint32Array {
     let padded = new Uint8Array(blockCount * 64);
     padded.set(data);
     padded[data.length] = 128;
-    // console.log(padded);
     let blocks = new DataView(padded.buffer);
-    blocks.setBigUint64(padded.length - 8, BigInt(data.length), true);
+    blocks.setBigUint64(padded.length - 8, BigInt(data.length * 8), true);
+    debug('padded:', padded);
+    console.log('');
     for (let block = 0; block < blockCount; block++) {
         let a = out[0];
         let b = out[1];
         let c = out[2];
         let d = out[3];
+        debug2('initial abcd:', a, b, c, d);
+        console.log('');
         for (let i = 0; i < 64; i++) {
             let f: number;
-            if (i < 20) {
+            if (i < 16) {
                 f = (b & c) | (~b & d);
-            } else if (i < 40) {
-                f = (b & c) | (b & ~d);
-            } else if (i < 60) {
+            } else if (i < 32) {
+                f = (b & d) | (c & ~d);
+            } else if (i < 48) {
                 f = b ^ c ^ d;
             } else {
-                f = b ^ (c | ~d);
+                f = c ^ (b | ~d);
             }
-            f = (f + a + MD5_CONSTANTS[i] + blocks.getUint32((block * 4 + MD5_BLOCKS[i]) * 4)) | 0;
+            f = (f + a + MD5_CONSTANTS[i] + blocks.getUint32((block * 4 + MD5_BLOCKS[i]) * 4, true)) | 0;
             a = d;
             d = c;
             c = b;
-            b += (f << MD5_SHIFTS[i]) | (f >>> (-MD5_SHIFTS[i] & ((1 << 31) * 2 - 1)));
+            b = (b + ((f << MD5_SHIFTS[i]) | (f >>> (32 - MD5_SHIFTS[i])))) | 0;
+            if (i % 4 === 0) {
+                debug2('round ' + i + ':', b, c, d, a);
+            } else if (i % 4 === 1) {
+                debug2('round ' + i + ':', c, d, a, b);
+            } else if (i % 4 === 2) {
+                debug2('round ' + i + ':', d, a, b, c);
+            } else {
+                debug2('round ' + i + ':', a, b, c, d);
+                console.log('');
+            }
         }
         out[0] += a;
         out[1] += b;
         out[2] += c;
         out[3] += d;
     }
-    return out;
+    let actualOut = new Uint8Array(16);
+    let view = new DataView(actualOut.buffer);
+    for (let i = 0; i < 4; i++) {
+        view.setUint32(i * 4, out[i], true);
+    }
+    return actualOut;
 }
 
-// console.log(Array.from(md5(new Uint8Array(0))).map(x => x.toString(16).padStart(8, '0')).join(''));
+console.log(Array.from(md5(new Uint8Array(0))).map(x => x.toString(16).padStart(2, '0')).join(' '));
 
 export function identify(p: Pattern, limit: number): Identified {
     let type = findType(p, limit);
-    let minmax: {min: string, max: string};
-    if (p instanceof MAPPattern) {
-        minmax = mapMinmax(p, type);
-    } else {
-        throw new Error('Unknown Pattern subclass');
-    }
     let apgcode: string;
     let desc: string;
     if (type.disp) {
@@ -284,5 +237,5 @@ export function identify(p: Pattern, limit: number): Identified {
             apgcode = 'zz_QAUDRATIC';
         }
     }
-    return {rle: p.toRLE(), apgcode, desc, ...type, ...minmax};
+    return {rle: p.toRLE(), apgcode, desc, ...type};
 }
