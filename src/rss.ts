7724110aa6480d6e4dea638689c0aef9a636bb3f
speedydelete <speedykorok777@gmail.com>
2025-12-14 17:36:11 -0500

    stuff

---
+++ src/rss.ts
@@ -0,0 +1,19 @@
+
+const CHECK_TRS = [
+    'B2c', 'B2e', 'B2i', 'B2k', 'B2n',
+    'B3a', 'B3c', 'B3e', 'B3i', 'B3j', 'B3k', 'B3n', 'B3q', 'B3r', 'B3y',
+    'B4a', 'B4c', 'B4e', 'B4i', 'B4j', 'B4k', 'B4n', 'B4q', 'B4r', 'B4t', 'B4w', 'B4y', 'B4z',
+    'B5a', 'B5c', 'B5e', 'B5i', 'B5j', 'B5k', 'B5n', 'B5q', 'B5r', 'B5y',
+    'B6a', 'B6c', 'B6e', 'B6i', 'B6k', 'B6n',
+    'B7c', 'B7e',
+    'B8c',
+    'S0c',
+    'S1c', 'S1e',
+    'S2a', 'S2c', 'S2e', 'S2i', 'S2k', 'S2n',
+    'S3a', 'S3c', 'S3e', 'S3i', 'S3j', 'S3k', 'S3n', 'S3q', 'S3r', 'S3y',
+    'S4a', 'S4c', 'S4e', 'S4i', 'S4j', 'S4k', 'S4n', 'S4q', 'S4r', 'S4t', 'S4w', 'S4y', 'S4z',
+    'S5a', 'S5c', 'S5e', 'S5i', 'S5j', 'S5k', 'S5n', 'S5q', 'S5r', 'S5y',
+    'S6a', 'S6c', 'S6e', 'S6i', 'S6k', 'S6n',
+    'S7c', 'S7e',
+    'S8c',
+];
