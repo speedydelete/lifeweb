92bc966e4b0f278915ec1d7b4e097b9b5b8455f4
speedydelete <speedykorok777@gmail.com>
2025-12-28 10:32:33 -0500

    oops 184

--- src/pattern.ts
+++ src/pattern.ts
@@ -144,7 +144,7 @@ export const SYMMETRY_LEAST: {[K in RuleSymmetry]: {[L in RuleSymmetry]: RuleSym
     'C2': {
         'C1': 'C1',
         'C2': 'C2',
-        'C4': 'C4',
+        'C4': 'C2',
         'D2-': 'C1',
         'D2|': 'C1',
         'D2x': 'C1',
