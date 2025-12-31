f20336792dfce79b6dd9630f9a92d6c8d54732d6
speedydelete <speedykorok777@gmail.com>
2025-12-31 14:43:24 -0500

    oops 217

--- src/ruleloader_bgolly.ts
+++ src/ruleloader_bgolly.ts
@@ -69,7 +69,7 @@ export class RuleLoaderBgollyPattern extends DataPattern {
                 this.states = value.states;
                 this.ruleSymmetry = value.symmetry;
             } else {
-                let {states, symmetry} = getAtRuleStatesAndSymmetries(fs.readFileSync(join(dir, override)).toString());
+                let {states, symmetry} = getAtRuleStatesAndSymmetries(fs.readFileSync(join(dir, override + '.rule')).toString());
                 this.states = states;
                 this.ruleSymmetry = symmetry;
                 rules.set(override, {name: override, states, symmetry});      
