c544f0754f44ba3d76b40002acc6a2444b5dfb25
speedydelete <speedykorok777@gmail.com>
2026-01-29 20:07:51 -0500

    oops 407

--- src/uc/config.ts
+++ src/uc/config.ts
@@ -61,7 +61,7 @@ const CHANNEL_INFO: {[key: string]: ChannelInfo} = {
         start: ['11', 9],
         elbows: {
             xs2_11: {0: false},
-            xs2_3: {7: true},
+            xs2_3: {[-7]: true},
         }
     },
 };
