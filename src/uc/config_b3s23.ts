eba841d50a4248e5808902160c88b1dd8c30e93d
speedydelete <speedykorok777@gmail.com>
2026-02-15 18:22:00 -0500

    fine 2

--- src/uc/config_b3s23.ts
+++ src/uc/config_b3s23.ts
@@ -196,7 +196,7 @@ const CHANNEL_INFO: {[key: string]: ChannelInfo} = {
 // the maximum number of generations it should take a glider to get to the object (dependant on GLIDER_TARGET_SPACING)
 const MAX_WAIT_GENERATIONS = 60;
 // the maximum number of generations it can take a collision to stabilize, collisions past this are reported as "unknown"
-const MAX_GENERATIONS = 512;
+const MAX_GENERATIONS = 384;
 // the number of population periods to repeat to make sure it's stable
 const PERIOD_SECURITY = 16;
 // this is optional, they enable a RSS-like period filter (see https://conwaylife.com/forums/viewtopic.php?f=9&t=7098&p=222961#p222961) that can help, set to null to disable
