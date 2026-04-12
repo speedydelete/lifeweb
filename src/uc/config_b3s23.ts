7a9e60c3e93ae80bdc38bae993a2b6fdeb12ef6f
speedydelete <speedykorok777@gmail.com>
2026-04-12 02:59:11 -0400

    ok...

--- src/uc/config_b3s23.ts
+++ src/uc/config_b3s23.ts
@@ -171,7 +171,7 @@ const SALVO_INFO: {[key: string]: SalvoInfo} = {
         period: 1,
         intermediateObjects: ['xs4_33', 'xs6_696', 'xs6_2552', 'xs7_2596', 'xs7_4a96', 'xs7_69a4', 'xs7_6952', 'xs5_253', 'xs5_256', 'xs5_652', 'xs5_352', 'xs6_356', 'xs6_653', 'xs4_252', 'xs8_6996', 'xs7_25ac', 'xs7_ca52', 'xs7_35a4', 'xs7_4a53'],
         laneLimit: 128,
-        maxRecipes: 5,
+        maxRecipes: 2,
         restriction: [[[2, 0]]],
     },
 
