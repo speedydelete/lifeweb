0ba1004efbb7a00418efb0bd15b89cb89efe5e14
speedydelete <speedykorok777@gmail.com>
2026-05-10 16:33:42 -0400

    oops 676

--- src/core/bounded.ts
+++ src/core/bounded.ts
@@ -213,9 +213,6 @@ export class TorusDataPattern extends DataPattern {
         }
         p.xOffset = 0;
         p.yOffset = 0;
-        throw new Error(xOffset + ' ' + yOffset + ' ' + this.toRLE());
-        // this.xOffset = xOffset;
-        // this.yOffset = yOffset;
         this.generation++;
     }
 
