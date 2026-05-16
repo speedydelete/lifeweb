e488f4ff379966f22f4bef065d1f555aeef5d33b
speedydelete <speedykorok777@gmail.com>
2026-05-16 13:25:23 -0400

    rework build process

--- build.mjs
+++ build.mjs
@@ -1,41 +1,78 @@
 
-import * as fs from 'node:fs';
+import {join} from 'node:path';
+import * as fs from 'node:fs/promises';
 import * as esbuild from 'esbuild';
+import minify from '@minify-html/node';
 
-const TARGETS = {
 
-    'main': ['src/index.ts', 'lifeweb.js'],
-
-    'editor': ['src/editor.ts', 'editor/index.js', () => {
-        let data = fs.readFileSync('src/editor.html').toString();
-        let match = data.match(/var BUILD_NUMBER=(\d+);/);
-        if (!match) {
-            throw new Error(`No build number!`);
-        }
-        data = data.replace(match[0], `var BUILD_NUMBER=${parseInt(match[1]) + 1};`);
-        fs.writeFileSync('src/editor.html', data);
-        fs.writeFileSync('editor/index.html', data);
-        fs.writeFileSync('editor/stdlib.rpf', fs.readFileSync('src/stdlib.rpf'));
-    }],
-
-};
-
-let target = process.argv[2];
-let [entry, out, func] = TARGETS[target];
-
-if (func) {
-    func();
+function path(value) {
+    return join(import.meta.dirname, value);
 }
 
-esbuild.build({
-    entryPoints: [entry],
-    bundle: true,
-    outfile: out,
-    minifyWhitespace: true,
-    minifySyntax: true,
-    sourcemap: true,
-    format: 'esm',
-    target: ['chrome85', 'edge85', 'safari14.1', 'firefox77', 'opera71'],
-    external: ['node:path'],
-    treeShaking: false,
-});
+
+async function buildMain() {
+    await esbuild.build({
+        entryPoints: [path('src/index.ts')],
+        bundle: true,
+        outfile: path('lifeweb.js'),
+        format: 'esm',
+        sourcemap: true,
+        target: ['chrome85', 'edge85', 'safari14.1', 'firefox77', 'opera71'],
+        external: ['node:path'],
+        treeShaking: false,
+        minifyWhitespace: true,
+        minifySyntax: true,
+    });
+}
+
+async function buildEditor() {
+    buildMain();
+    let data = (await fs.readFile(path('src/editor.html'))).toString();
+    let match = data.match(/var BUILD_NUMBER=(\d+);/);
+    if (!match) {
+        console.error(`Error: No build number in src/editor.html!`);
+        throw new Error(`No build number!`);
+    }
+    data = data.replace(match[0], `var BUILD_NUMBER=${parseInt(match[1]) + 1};`);
+    await fs.writeFile(path('src/editor.html'), data);
+    await fs.writeFile(path('editor/index.html'), await minify.minify(Buffer.from(data, 'utf-8'), {
+        keep_html_and_head_opening_tags: true,
+        minify_css: true,
+    }));
+    await fs.writeFile(path('editor/stdlib.rpf'), await fs.readFile('src/stdlib.rpf'));
+    await esbuild.build({
+        entryPoints: [path('src/editor.ts')],
+        bundle: true,
+        outfile: path('editor/index.js'),
+        format: 'esm',
+        sourcemap: true,
+        target: ['chrome85', 'edge85', 'safari14.1', 'firefox77', 'opera71'],
+        external: ['node:path'],
+        treeShaking: false,
+        minifyWhitespace: true,
+        minifySyntax: true,
+        plugins: [
+            {
+                name: 'lifeweb-core-alias',
+                setup(build) {
+                    build.onResolve({filter: /\/core\/index\.js$/}, () => ({path: '../lifeweb.js', external: true}));
+                }
+            }
+        ]
+    });
+}
+
+// const TARGETS = {
+//     main: buildMain,
+//     editor: buildEditor,
+// };
+
+
+// let target = process.argv[2];
+// if (!(target in TARGETS)) {
+//     console.error(`Error: Invalid target: '${target}' (valid: ${Object.keys(targets).join(', ')})`);
+//     process.exit(1);
+// }
+// TARGETS[target]();
+
+buildEditor();
