51c3c490ec5770b0f672027a32fcc5be114549b2
speedydelete <speedykorok777@gmail.com>
2026-05-22 17:03:35 -0400

    ok...

--- src/uc/search_mss.ts
+++ src/uc/search_mss.ts
@@ -1,26 +1,33 @@
 
-import {c, StillLife, Spaceship, loadRecipes} from './base.js';
+import {c, base, StillLife, Spaceship, loadRecipes} from './base.js';
 
 
 const TYPE = 'Slow salvo';
+const START_OBJECT = 'xs2_11';
+const END_OBJECT = 'xs2_11';
+const END_LANE = 0;
 const SALVO = [0, -2, -8, -9, 3, 3, 1, 7, 11, 7, -17, -11, -3, -12, -2, -5];
 const DIR: string = 'SW';
-const MAX_GPG = Infinity;
-const BEAM_WIDTH = 8192;
+const BEAM_WIDTH = 65536;
 const MIN_LANE = -16;
 const MAX_LANE = 16;
+const CLOSENESS_OFFSET = -5;
 
 let info = c.SALVO_INFO[TYPE];
 let recipes = (await loadRecipes()).salvos['Slow salvo'];
 
 interface State {
+    key: string;
     elbow: string;
     x: number;
     y: number;
+    closeness: number;
     gliders: number[];
     emitted: number;
     sinceLastGlider: number;
 }
+
+const SIZES: {[key: string]: number} = {};
 
 // let bad = new Set<string>();
 
@@ -59,15 +66,17 @@ function addToState(state: State): State[] {
         if (!(sl.code in recipes.searchResults)) {
             continue;
         }
-        let newX = state.x + sl.x;
-        let newY = state.y + sl.y;
+        let x = state.x + sl.x;
+        let y = state.y + sl.y;
+        let emitted = state.emitted;
         if (ship) {
-            let x = newX - ship.x;
-            let y = newY - ship.y;
-            let lane = DIR === 'NW' || DIR === 'SE' ? x - y : x + y;
+            let shipX = x - ship.x;
+            let shipY = y - ship.y;
+            let lane = DIR === 'NW' || DIR === 'SE' ? shipX - shipY : shipX + shipY;
             if (lane !== SALVO[state.emitted]) {
                 continue;
             }
+            emitted++;
         }
         // if (bad.has(sl.code + ' ' + (newY - newX))) {
         //     continue;
@@ -75,21 +84,30 @@ function addToState(state: State): State[] {
         // found = true;
         let gliders = state.gliders.slice();
         gliders.push(lane);
+        let closeness: number;
+        if (!(sl.code in SIZES)) {
+            let p = base.loadApgcode(sl.code.slice(sl.code.indexOf('_'))).shrinkToFit();
+            let value = Math.floor((p.height + p.width) / 2);
+            closeness = x + y + value;
+            SIZES[sl.code] = value;
+        } else {
+            closeness = x + y + SIZES[sl.code];
+        }
+        if (SALVO[emitted] === undefined) {
+            closeness = 0;
+        } else {
+            closeness = Math.abs(SALVO[emitted] - (closeness + CLOSENESS_OFFSET));
+        }
         let value: State = {
+            key: sl.code + ' ' + x + ' ' + y + ' ' + emitted,
             elbow: sl.code,
-            x: newX,
-            y: newY,
-            gliders: gliders,
-            emitted: state.emitted,
-            sinceLastGlider: state.sinceLastGlider + 1,
+            x,
+            y,
+            closeness,
+            gliders,
+            emitted,
+            sinceLastGlider: ship ? 0 : state.sinceLastGlider + 1,
         };
-        if (ship) {
-            value.emitted++;
-            value.sinceLastGlider = 0;
-        }
-        if (value.sinceLastGlider > MAX_GPG) {
-            continue;
-        }
         out.push(value);
     }
     // if (!found) {
@@ -99,9 +117,11 @@ function addToState(state: State): State[] {
 }
 
 let prevLayer: State[] = [{
-    elbow: 'xs2_11',
+    key: START_OBJECT + ' 0 0 0',
+    elbow: START_OBJECT,
     x: 0,
     y: 0,
+    closeness: Math.abs(SALVO[0]) + CLOSENESS_OFFSET,
     gliders: [],
     emitted: 0,
     sinceLastGlider: 0,
@@ -110,8 +130,9 @@ let prevLayer: State[] = [{
 
 let depth = 1;
 while (true) {
-    let nextLayer = [];
-    let best = 0;
+    let data = new Map<string, State>();
+    let bestEmitted = 0;
+    let bestCloseness = Infinity;
     console.log(`Searching depth ${depth}`);
     for (let state of prevLayer) {
         for (let value of addToState(state)) {
@@ -119,20 +140,27 @@ while (true) {
                 console.log(`Solution found: ${value.gliders.join(', ')}`);
                 process.exit(0);
             }
-            best = Math.max(best, value.emitted);
+            bestEmitted = Math.max(bestEmitted, value.emitted);
+            bestCloseness = Math.min(bestCloseness, value.closeness);
+            if (!data.has(value.key)) {
+                data.set(value.key, value);
+            }
         }
-        nextLayer.push(...addToState(state));
     }
-    let total = nextLayer.length;
-    nextLayer = nextLayer.sort((x, y) => {
+    let total = data.size;
+    let nextLayer = Array.from(data.values()).sort((x, y) => {
         if (x.emitted === y.emitted) {
-            return y.gliders.length - x.gliders.length;
+            if (x.closeness === y.closeness) {
+                return x.gliders.length - y.gliders.length;
+            } else {
+                return x.closeness - y.closeness;
+            }
         } else {
             return y.emitted - x.emitted;
         }
     }).slice(0, BEAM_WIDTH);
     // console.log(nextLayer.map(x => `${x.elbow} (${x.x}, ${x.y}): ${x.gliders.join(', ')}`).join('\n'));
-    console.log(`Depth ${depth} complete (total results: ${total}, best emitted glider count: ${best})`);
+    console.log(`Depth ${depth} complete (total results: ${total}, best emitted glider count: ${bestEmitted}, best closeness: ${bestCloseness})`);
     if (nextLayer.length === 0) {
         console.log(`Search exhausted`);
         break;
