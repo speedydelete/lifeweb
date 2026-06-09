b47b6e6f08539e7a5ee1a84cdcc3af3a1970d6eb
speedydelete <speedykorok777@gmail.com>
2026-06-09 14:36:20 -0400

    test

--- src/core/super.ts
+++ src/core/super.ts
@@ -271,6 +271,7 @@ export class SuperPattern extends DataPattern {
                     } else if (oldValue === 22) {
                         out[i] = newValue % 2 === 1 ? newValue : (cells.includes(20) ? 20 : 22);
                     } else if (oldValue === 24) {
+                        throw new Error('hi');
                         out[i] = cells.some(x => x % 2 === 1) ? 18 : 24;
                     }
                 }
