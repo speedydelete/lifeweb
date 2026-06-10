444af14780f4edafea850ad00b983a10b6505bab
speedydelete <speedykorok777@gmail.com>
2026-06-10 12:39:06 -0400

    test 2

--- src/core/separate.ts
+++ src/core/separate.ts
@@ -59,7 +59,7 @@ export class Separator<T extends Pattern = Pattern> extends DataPattern {
         // this is used inside the loop
         let groupsInNH = new Set<number>();
         if (eval('true')) {
-            throw new Error(this.data.join(' '));
+            throw new Error(data.join(' '));
         }
         for (let y = 0; y < this.height; y++) {
             for (let x = 0; x < this.width; x++) {
