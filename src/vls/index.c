61dc3f614c186cac1f5307a8f096d833d22802f8
speedydelete <speedykorok777@gmail.com>
2026-06-25 18:50:25 -0400

    oops 878

--- src/vls/index.c
+++ src/vls/index.c
@@ -91,7 +91,7 @@ static inline void actual_run_depth(int depth, cell* cell, cell_value_t value) {
         #endif
     #if MULTI_RULE
     } else if (rule_dependent_tr != -1) {
-        #if SPECIAL_PHASE_0_POP
+        #ifdef SPECIAL_PHASE_0_POP
         if (!pop_frame()) {
             return;
         }
@@ -114,7 +114,7 @@ static inline void actual_run_depth(int depth, cell* cell, cell_value_t value) {
         return;
     #endif
     }
-    #if SPECIAL_PHASE_0_POP
+    #ifdef SPECIAL_PHASE_0_POP
     if (!pop_frame()) {
         return;
     }
