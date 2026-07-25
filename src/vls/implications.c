0e0075ce994b079f8f6db7d41502d03221d980ba
speedydelete <speedykorok777@gmail.com>
2026-07-25 14:21:58 -0400

    SERIOUSLY

--- src/vls/implications.c
+++ src/vls/implications.c
@@ -319,17 +319,18 @@ static inline bool check_implication(cell* cell) {
     return true;
 }
 
-#define check_implications(cell) \
-    (check_implication((cell)) \
-    && check_implication((cell)->prev) \
-    && check_implication((cell)->nw) \
-    && check_implication((cell)->n) \
-    && check_implication((cell)->ne) \
-    && check_implication((cell)->w) \
-    && check_implication((cell)->e) \
-    && check_implication((cell)->sw) \
-    && check_implication((cell)->s) \
-    && check_implication((cell)->se))
+static inline bool check_implications(cell* cell) {
+    return check_implication((cell))
+        && check_implication((cell)->prev)
+        && check_implication((cell)->nw)
+        && check_implication((cell)->n)
+        && check_implication((cell)->ne)
+        && check_implication((cell)->w)
+        && check_implication((cell)->e)
+        && check_implication((cell)->sw)
+        && check_implication((cell)->s)
+        && check_implication((cell)->se);
+}
 
 
 #if VARIABLES
--- src/vls/index.ts
+++ src/vls/index.ts
@@ -1341,7 +1341,7 @@ export async function main() {
     let [options, code] = await transformCode(process.argv, source);
     await fs.writeFile(getPath('src/vls/params2.h'), code);
     try {
-        let command = `gcc --std=c2x -Wall -Wextra -Werror -Wpedantic -Wno-unused-function -Wno-unknown-pragmas ${options['profile'] ? '-pg -O3' : (options['g'] || options['gdb'] ? '-g -O0' : '-O3')} -march=native -mtune=native -flto -fno-stack-protector -fomit-frame-pointer -o '${execPath}' '${getPath('src/vls/index.c')}'`;
+        let command = `gcc --std=c2x -Wall -Wextra -Werror -Wpedantic -Wno-unused-function -Wno-unknown-pragmas ${options['profile'] ? '-pg -O3' : (options['g'] || options['gdb'] ? '-g -O3' : '-O3')} -march=native -mtune=native -flto -fno-stack-protector -fomit-frame-pointer -o '${execPath}' '${getPath('src/vls/index.c')}'`;
         execSync(command, {stdio: 'inherit'});
         if (options['g'] && !options['gdb']) {
             return;
