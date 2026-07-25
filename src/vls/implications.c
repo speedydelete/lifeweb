ed5e023a2d5f220affcf7355cb76b0a468b3d528
speedydelete <speedykorok777@gmail.com>
2026-07-25 14:23:36 -0400

    hmm

--- src/vls/implications.c
+++ src/vls/implications.c
@@ -138,6 +138,7 @@ int32_t implications[1048576];
 #define IMPLICATIONDPRINTF(...)
 #endif
 
+__attribute__((always_inline))
 static inline int32_t get_implication(uint32_t tr) {
     // invalid transition
     for (int i = 0; i < 20; i += 2) {
