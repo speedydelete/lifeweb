0a7bed771c276c595cb9ed27b9474733fdc30de8
speedydelete <speedykorok777@gmail.com>
2026-08-10 13:20:32 -0400

    test wtf

--- src/core/identify.ts
+++ src/core/identify.ts
@@ -58,6 +58,7 @@ export function identifyPeriodic(p: Pattern, limit: number, acceptStabilized: bo
                     }
                 }
                 if (checkLinear) {
+                    console.log('checking linear');
                     for (let period = 1; period < Math.floor((i - j) / 16); period++) {
                         let diff = pop - pops[pops.length - period];
                         if (diff === 0) {
