3dc81bb7dd7d61855768b37ccdbc35589b961cbb
speedydelete <speedykorok777@gmail.com>
2026-09-06 13:17:10 -0400

    fine luveel

--- src/vls/index.ts
+++ src/vls/index.ts
@@ -1329,7 +1329,7 @@ return [options, out.join('\n')];
 }
 
 
-const FLAGS = `--std=c23 -Wall -Wextra -Werror -Wpedantic -Wno-gnu-binary-literal -Wno-unused-function -Wno-unknown-pragmas -g -O3 -march=native -mtune=native -flto -fno-stack-protector -fomit-frame-pointer`;
+const FLAGS = `--std=c2x -Wall -Wextra -Werror -Wpedantic -Wno-gnu-binary-literal -Wno-unused-function -Wno-unknown-pragmas -g -O3 -march=native -mtune=native -flto -fno-stack-protector -fomit-frame-pointer`;
 
 const PROFILE_SECONDS = 5;
 
