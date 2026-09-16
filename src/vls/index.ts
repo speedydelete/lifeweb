6cb5379fe83ec6e58e4148ba9ce095303a87cac0
speedydelete <speedykorok777@gmail.com>
2026-09-16 19:32:04 -0400

    LUVEEL I SWEAR

--- src/vls/index.ts
+++ src/vls/index.ts
@@ -923,10 +923,7 @@ export async function main() {
     try {
         let command = options['clang'] ? `clang -std=c23` : `gcc -std=c2x`;
         // strict mode
-        command += ` -Wall -Werror -Wpedantic -Wextra -Wno-unused-function -Wno-unknown-pragmas`;
-        if (options['clang']) {
-            command += ` -Wno-gnu-binary-literal`;
-        }
+        command += ` -Wall -Werror -Wpedantic -Wextra -Wno-gnu-binary-literal -Wno-unused-function -Wno-unknown-pragmas`;
         // features
         command += ` -g`;
         if (!options['no-optimize']) {
