47a42659f479256f9bc72ed32fb45a0c5cefdead
speedydelete <speedykorok777@gmail.com>
2026-09-23 20:16:08 -0400

    ok NOW it works??? maybe???

--- src/vls/custom/signal.c
+++ src/vls/custom/signal.c
@@ -7,16 +7,22 @@
 
 
 const Index drifter_cells[][2] = {
-    {7, 7},
-    {8, 8},
     {9, 9},
+    {9, 10},
+    {9, 11},
+    {10, 9},
+    {10, 10},
+    {10, 11},
+    {11, 9},
+    {11, 10},
+    {11, 11},
 };
 
 
 #define CUSTOM_INIT true
 #define CUSTOM_SOLUTION_FILTERING false
-#define CUSTOM_PRUNING false
-#define CUSTOM_PRUNING_ON_CELL_SET true
+#define CUSTOM_PRUNING true
+#define CUSTOM_PRUNING_ON_CELL_SET false
 
 
 #if CUSTOM_INIT
@@ -33,7 +39,16 @@ static inline bool custom_solution_filter([[maybe_unused]] DynamicGrid grid) {
 
 #if CUSTOM_PRUNING
 static inline bool custom_prune([[maybe_unused]] Depth depth, [[maybe_unused]] Cell* cell) {
-    return true;
+    for (size_t i = 0; i < sizeof(drifter_cells) / sizeof(drifter_cells[0]); i++) {
+        Index x = drifter_cells[i][0] + 2;
+        Index y = drifter_cells[i][1] + 2;
+        CellValue value1 = get_cell(0, x, y)->value;
+        CellValue value2 = get_cell(1, x, y)->value;
+        if (!(is_known(value1) && is_known(value2) && value1 == value2)) {
+            return true;
+        }
+    }
+    return false;
 }
 #endif
 
@@ -43,14 +58,12 @@ static inline bool custom_prune_on_cell_set([[maybe_unused]] Cell* cell) {
     for (size_t i = 0; i < sizeof(drifter_cells) / sizeof(drifter_cells[0]); i++) {
         Index x = drifter_cells[i][0] + 2;
         Index y = drifter_cells[i][1] + 2;
-        if (cell->x == x && cell->y == y) {
-            CellValue value1 = get_cell(0, x, y)->value;
-            CellValue value2 = get_cell(1, x, y)->value;
-            if (is_known(value1) && is_known(value2) && value1 == value2) {
-                return false;
-            }
+        CellValue value1 = get_cell(0, x, y)->value;
+        CellValue value2 = get_cell(1, x, y)->value;
+        if (is_known(value1) && is_known(value2) && value1 != value2) {
+            return true;
         }
     }
-    return true;
+    return false;
 }
 #endif
